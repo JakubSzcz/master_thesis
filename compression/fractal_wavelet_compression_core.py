compression rate calculation, small logginng fixes

compression/fractal_wavelet_compression_core.py
@@ -10,7 +10,6 @@ import util.math as mymath
 
 
 # TODO CUSTOM OVERLAPPING - as for now only Cyclic buffer supported
-# TODO disable logging parameter
 # TODO IFS on lower layers
 # TODO investigate other metric not only, L2 norm/dot product
 # TODO asynchronous encoding
@@ -36,7 +35,7 @@ def wavelet_decomposition(signal: np.ndarray, wavelet_family: str, decomposition
     assert max_decomp >= decomposition_level, \
         f"Desired wavelet decomposition level is too high. Maximum level is {max_decomp}."
     if not suppress_logs:
-        print("starting wavelet decomposition...")
+        print("Starting wavelet decomposition...")
     # DWT on X
     return pywt.wavedec(signal, wavelet_family, level=decomposition_level)
 
@@ -58,7 +57,7 @@ def encode_wavelets(wavelets_coefficients: list, r_blocks_level: int, block_heig
     """
     start_time_enc = time.time()
     if not suppress_logs:
-        print("starting encoding...")
+        print("Starting encoding...")
 
     # prepare blocks
     a_coeffs = wavelets_coefficients[1:]
@@ -90,8 +89,8 @@ def encode_wavelets(wavelets_coefficients: list, r_blocks_level: int, block_heig
             uniq_d.add(d_index)
         if not suppress_logs:
             print("\rProgress: 100%.", flush=True)
-            print(f"d used: {len(uniq_d)}/{n_domain}")
-            print(f"encoding finished with {round(time.time() - start_time_enc, 2)}s.")
+            print(f"Unique d used: {len(uniq_d)}/{n_domain}")
+            print(f"Encoding finished with {round(time.time() - start_time_enc, 2)}s.")
 
     # BRUTEFORCE TYPE
     # encoded parameters for each range block
@@ -107,8 +106,8 @@ def encode_wavelets(wavelets_coefficients: list, r_blocks_level: int, block_heig
             uniq_d.add(d_index)
         if not suppress_logs:
             print("\rProgress: 100%.", flush=True)
-            print(f"d used: {len(uniq_d)}/{n_domain}")
-            print(f"encoding finished with {round(time.time() - start_time_enc, 2)}s.")
+            print(f"Unique d used: {len(uniq_d)}/{n_domain}")
+            print(f"Encoding finished with {round(time.time() - start_time_enc, 2)}s.")
 
     return coeffs_to_be_stored, np.array(codded)
 
@@ -128,7 +127,7 @@ def decode(coded: tuple, wavelet_family: str, r_blocks_level: int, block_height:
     :return: reconstructed decoded signal
     """
     if not suppress_logs:
-        print("starting decoding...")
+        print("Starting decoding...")
     start_time_dec = time.time()
     to_be_stored, coded_blocks = coded
     if len(coded_blocks[0]) == 2:
@@ -153,7 +152,7 @@ def decode(coded: tuple, wavelet_family: str, r_blocks_level: int, block_height:
 
     reconstructed_signal = pywt.waverec(decoded, wavelet_family)
     if not suppress_logs:
-        print(f"decoding finished with {round(time.time() - start_time_dec, 2)}s.")
+        print(f"Decoding finished with {round(time.time() - start_time_dec, 2)}s.")
     return np.array(reconstructed_signal)
 
 
@@ -230,7 +229,6 @@ def set_sub_block(starting_ind: int, samples_to_add: int, org_block: np.ndarray,
     return next_starting_ind
 
 
-# TODO make for adjust compliance
 def generate_blocks_matrix(blocks_level: int, block_height: int, coefficients: np.ndarray) -> np.ndarray:
     """
     Generates blocks matrix at some level from root up to block_height from coefficients array (subtrees)
@@ -266,11 +264,11 @@ def generate_r_d(r_blocks_level: int, block_height: int, coefficients: np.ndarra
     """
     start_time_blocks = time.time()
     if not suppress_logs:
-        print("starting generating blocks...")
+        print("Starting generating blocks...")
     r = generate_blocks_matrix(r_blocks_level, block_height, coefficients)
     d = generate_blocks_matrix(r_blocks_level - 1, block_height, coefficients)
     if not suppress_logs:
-        print(f"blocks generation finished with {round(time.time() - start_time_blocks, 2)}s.")
+        print(f"Blocks generation finished with {round(time.time() - start_time_blocks, 2)}s.")
     return r, d
 
 

util/common.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pywt
+
 import util.math as mymath
 from util.wavFile import read_wav_file
 
@@ -49,6 +51,42 @@ def print_mesures(reconstructed_signal: np.ndarray, original_signal: np.ndarray)
     print(f"PSNR = {round(mymath.calculate_psnr(mse), 3)} dB")
 
 
+def get_compression_rate(n_samples: int, block_height: int, wavelet_family: str, stores_only_alpha: bool = False,
+                         bit_wise: bool = True, bit_depth: int = 16):
+    """
+    Prints compression rate
+    :param n_samples: number of samples in original signal
+    :param block_height: how many wavelet decomposition levels are to be encoded
+    :param wavelet_family: family of wavelets e.g. db10
+    :param stores_only_alpha: if only alpha parameter of affine transforms were saved
+    :param bit_wise: flag indicating to consider compression rate in bytes, not samples numbers
+    :param bit_depth: how many bits per single sample were used in original signal
+    :return: calculated compression rate
+    """
+
+    filter_len = pywt.Wavelet(wavelet_family).dec_len
+    n_ifs_parameters = n_samples
+    for i in range(1, block_height + 1):
+        n_ifs_parameters = (n_ifs_parameters + filter_len - 1) // 2
+
+    coefficients_to_be_stored = (n_ifs_parameters + filter_len - 1) // 2 * 2
+
+    n_ifs_parameters = n_ifs_parameters * 2 if stores_only_alpha else n_ifs_parameters * 3
+    # TODO consider storing floats on 32 bits not 64
+    # assuming float -> 64 bits
+    float_bits_size = 64
+    if bit_wise:
+        n_samples *= bit_depth * 8
+        coefficients_to_be_stored *= float_bits_size
+        n_ifs_parameters *= float_bits_size
+    compression_rate = n_samples / (n_ifs_parameters + coefficients_to_be_stored)
+    to_print = f"Compression rate = {round(compression_rate, 3)}"
+    if bit_wise:
+        to_print += " (bit wise)"
+    print(to_print)
+    return compression_rate
+
+
 def print_signal(signal: list | np.ndarray, title: str, plot_ranges_size: int = None, title_appendix: str = None):
     """
     Prints single signal.
@@ -91,9 +129,11 @@ def read_example_file(n_samples: int = 2 ** 12, samples_offset: int = 100000, fi
         file = "../resources/en_speech.wav"
     audio_meta_data, signal = read_wav_file(file)
     audio_samplerate = audio_meta_data["fs"]
+    bit_depth = audio_meta_data["byteDepth"]
     # reads only one channel with offset
     signal = signal[0][samples_offset:n_samples + samples_offset]
 
-    print(f"Audio parameters: fs = {audio_samplerate}, samples = {n_samples}, "
-          f"duration = {round((1 / audio_samplerate) * n_samples, 2)}s.")
-    return signal, audio_samplerate
+    print(
+        f"Audio parameters: fs = {audio_samplerate}Hz, samples = {n_samples}, bit depth = {8 * bit_depth}bits/sample, "
+        f"duration = {round((1 / audio_samplerate) * n_samples, 2)}s.")
+    return signal, audio_samplerate, bit_depth