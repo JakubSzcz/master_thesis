39a319dd329d0d6f95de72be404f3ed623408573
JakubSzcz <jszczudlik@student.agh.edu.pl>
2025-06-21 16:18:35 +0200

    visualization fix v2

--- measurements/visualize_speach_vs_sound.py
+++ measurements/visualize_speach_vs_sound.py
@@ -5,21 +5,21 @@ sound_t, _, _ = common.read_example_file(n_samples=2 ** 13, file_type="sound",
                                          suppress_logs=True)
 speach_t, _, _ = common.read_example_file(n_samples=2 ** 13, file_type="speach",
                                           suppress_logs=True)
-plt.figure(figsize=(12, 7))
-plt.suptitle("Porównanie przebiegów czasowych sygnału typu muzyka oraz mowa", fontsize=16)
+plt.figure(figsize=(10, 6))
+plt.suptitle("Porównanie przebiegów czasowych sygnału typu muzyka oraz mowa", fontsize=19)
 
 plt.subplot(1, 2, 1)
 plt.title("Muzyka")
 plt.plot(sound_t, label="Muzyka")
-plt.xlabel("Numer próbki")
-plt.ylabel("Amplituda")
+plt.xlabel("Numer próbki", fontsize=14)
+plt.ylabel("Amplituda", fontsize=14)
 plt.grid(True)
 
 plt.subplot(1, 2, 2)
 plt.title("Mowa")
 plt.plot(speach_t, label="Mowa", color="orange")
-plt.xlabel("Numer próbki")
-plt.ylabel("Amplituda")
+plt.xlabel("Numer próbki", fontsize=14)
+plt.ylabel("Amplituda", fontsize=14)
 plt.grid(True)
 
 plt.tight_layout()
